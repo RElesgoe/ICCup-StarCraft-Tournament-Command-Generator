2423c7ebd9e235088d3591a041b4639ba1ea2d6c
xboi209 <xboi209@users.noreply.github.com>
2015-12-14 00:54:46 -0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,9 +1,27 @@
+/*
+ *	Userscript that allows ICCup admins to generate commands for tournaments
+ *	Copyright (C) 2015  xboi209 (xboi209@gmail.com)
+ *	
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // ==UserScript==
 // @name			ICCup StarCraft Tournament Command Generator
 // @description		Allows admins to generate commands on tournament grid pages   
 // @author			xboi209 (xboi209@gmail.com) 
 // @license			GNU General Public License v3 (http://www.gnu.org/licenses/gpl-3.0.en.html) 
-// @version			1.0
+// @version			1.1
 // @match			http://iccup.com/en/starcraft/tourney/grid/*.html
 // @match			http://iccup.com/starcraft/tourney/grid/*.html
 // @grant			none
@@ -23,6 +41,32 @@ function addJQuery(callback) {
 
 function main()
 {
+	function formatNum(num)
+	{
+		if (jQuery.isNumeric(num) == false)
+			return "";
+
+		var numberStr;
+		if (num == 1)
+		{
+			numberStr = "1st";
+		}
+		else if (num == 2)
+		{
+			numberStr = "2nd";
+		}
+		else if (num == 3)
+		{
+			numberStr = "3rd";
+		}
+		else
+		{
+			numberStr = num + "th";
+		}
+		
+		return numberStr;
+	}
+
 	var grid = jQuery("#t1x1");
 	if (grid.length == 0)
 		return; //2x2 tournaments aren't supported yet
@@ -65,6 +109,7 @@ function main()
 		place.push(placeWinners);
 	}
 	
+	//create pay-in container
 	jQuery("#bracket").after(`<div style='padding-left:25px'>
 		<h4>ICCup StarCraft Tournament Command Generator</h4>
 		<p id='xboi-cmdgen-payin-container'>Pay-in: <input type='number' min='0' step='1' value='0' id='xboi-cmdgen-payin-input'></p> <br>
@@ -74,30 +119,15 @@ function main()
 		<textarea rows='25' cols='80' spellcheck=false id='xboi-cmdgen-output'></textarea>
 	</div>`);
 	
+	//create prize container
 	for (var v = numRounds; v != 0; v--)
 	{
-		var numberStr;
-		if (v == 1)
-		{
-			numberStr = "1st";
-		}
-		else if (v == 2)
-		{
-			numberStr = "2nd";
-		}
-		else if (v == 3)
-		{
-			numberStr = "3rd";
-		}
-		else
-		{
-			numberStr = v + "th";
-		}
-		jQuery("#xboi-cmdgen-prize-container").prepend("<label>" + numberStr + " place prize: </label><input type='number' min='0' step='1' value='0' class='prize " + v + "'><br><br>");
+		jQuery("#xboi-cmdgen-prize-container").prepend("<label>" + formatNum(v) + " place prize: </label><input type='number' min='0' step='1' value='0' class='prize " + v + "'><br><br>");
 	}
 	
 	jQuery("#xboi-cmdgen-submit").click(function() {
-        jQuery("#xboi-cmdgen-output").val("").scrollTop();
+		//clear previous output and scroll to top every time button is clicked
+		jQuery("#xboi-cmdgen-output").val("").scrollTop();
         
 		for (var v = 1; v <= numRounds; v++)
 		{
@@ -106,28 +136,11 @@ function main()
 			});
 		}
 		
+		//repeat loop to separate the two commands
 		for (var v = 1; v <= numRounds; v++)
 		{
-			var numberStr;
-			if (v == 1)
-			{
-				numberStr = "1st";
-			}
-			else if (v == 2)
-			{
-				numberStr = "2nd";
-			}
-			else if (v == 3)
-			{
-				numberStr = "3rd";
-			}
-			else
-			{
-				numberStr = v + "th";
-			}
-			
 			place[numRounds - v].forEach(function(winner) {
-				jQuery("#xboi-cmdgen-output").val(jQuery("#xboi-cmdgen-output").val() + "/addwin1 " + winner +" 0 " + jQuery("#xboi-cmdgen-prize-container").children(".prize." + v).val() + " " + numberStr + " Place \n");
+				jQuery("#xboi-cmdgen-output").val(jQuery("#xboi-cmdgen-output").val() + "/addwin1 " + winner +" 0 " + jQuery("#xboi-cmdgen-prize-container").children(".prize." + v).val() + " " + formatNum(v) + " Place\n");
 			});
 		}
 
